e895a6124695aab4dba4b17d703c1444e3656040
Pa1-234 <150997748+Pa1-234@users.noreply.github.com>
2024-01-23 18:26:39 +0530

    Update HellojavaApplicationTests.java

--- service/hellojava/src/test/java/com/mydomain/HellojavaApplicationTests.java
+++ service/hellojava/src/test/java/com/mydomain/HellojavaApplicationTests.java
@@ -1,13 +1,1 @@
-package com.mydomain;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HellojavaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
